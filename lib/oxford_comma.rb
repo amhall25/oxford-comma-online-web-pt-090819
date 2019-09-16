21671e40e016d4f02b1ea62de08967d1b69c3d2a
alycia hall <amhall25@gmail.com>
2019-09-16 00:47:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  string=""
+  if array.length==1
+    string+= array[0]
+  elsif array.length==2
+    string+= array[0] + " and " + array[1]
+  else  
+  i=0
+  while i<array.length-1
+    string+= array[i]+", "
+  i+=1
+  end
+  string+="and "+array[-1]
+end
+  return string
 end
